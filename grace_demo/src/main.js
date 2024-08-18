3cf30c2c2dac64ea1396f650a33f8c9db364bdcf
GraceStar <luoyawen99@gmail.com>
2024-08-18 17:35:14 +0800

    feat：增加导航栏右侧汉堡svg图标，上传assets资源

--- grace_demo/src/main.js
+++ grace_demo/src/main.js
@@ -3,5 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import "./styles/index.scss";
+import mLibs from "./libs";
+import "virtual:svg-icons-register";
+import { useREM } from "./utils/flexible";
 
-createApp(App).use(router).mount("#app");
+useREM();
+
+createApp(App).use(router).use(mLibs).mount("#app");
