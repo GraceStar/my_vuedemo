e7cc8babed17accd5ffd0135d35c7d95dbfbe8d9
GraceStar <luoyawen99@gmail.com>
2024-08-18 15:50:47 +0800

    feat: 将navigation中的数据传递给视图

--- grace_demo/src/utils/request.js
+++ grace_demo/src/utils/request.js
@@ -5,4 +5,15 @@ const service = axios.create({
   timeout: 5000,
 });
 
+service.interceptors.request.use((response) => {
+  const { success, message, data } = response.data;
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data;
+  } else {
+    // TODO：业务错误
+    return Promise.reject(new Error(message));
+  }
+});
+
 export default service;
--- grace_demo/src/views/main/components/navigation/mobile/index.vue
+++ grace_demo/src/views/main/components/navigation/mobile/index.vue
@@ -1,3 +1,11 @@
 <template>
   <div>我是移动端下的 navigation</div>
 </template>
+<script setup>
+defineProps({
+  data: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
