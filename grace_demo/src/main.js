6b93b858bd7e20ed4112fe980e6b37d042fb5572
luoyw5 <luoyw5@essence.com.cn>
2024-09-10 16:41:50 +0800

    feat：增加tailwind

--- grace_demo/src/main.js
+++ grace_demo/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import './styles/index.scss';
+import { useREM } from './utils/flexible'
 
+// 设置 rem
+useREM()
 createApp(App).use(router).mount("#app");
--- grace_demo/src/utils/flexible.js
+++ grace_demo/src/utils/flexible.js
@@ -1,6 +1,17 @@
 import { computed } from "vue";
 import { useWindowSize } from "@vueuse/core";
+
 export const PC_DEVICE_WIDTH = 1280;
 export const isMobileTerminal = computed(() => {
   return useWindowSize().width.value < PC_DEVICE_WIDTH;
 });
+
+export const useREM = () => {
+  const MAX_FONT_SIZE = 40;
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector();
+    let fontSize = window.innerWidth/10;
+    fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE: fontSize;
+    html.style.fontSize = fontSize + 'px';
+  });
+}
