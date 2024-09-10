b91fda1942f3aa6211d1e144da09903d250482c7
luoyw5 <luoyw5@essence.com.cn>
2024-09-10 15:41:50 +0800

    feat：配置vite环境变量

--- grace_demo/src/utils/request.js
+++ grace_demo/src/utils/request.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 const service = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_BASE_API,
     timeout: 5000
 });
+
+console.log("call baseurl:" + import.meta.env.VITE_BASE_API);
 
 export default service;
