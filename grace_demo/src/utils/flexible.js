0959ab61f10daf84fcf13e804b2d2c2c0dc8bac7
GraceStar <luoyawen99@gmail.com>
2024-09-12 08:32:04 +0800

    tmp: 临时提交

--- grace_demo/src/utils/flexible.js
+++ grace_demo/src/utils/flexible.js
@@ -2,7 +2,8 @@ import { computed } from "vue";
 import { useWindowSize } from "@vueuse/core";
 export const PC_DEVICE_WIDTH = 1280;
 export const isMobileTerminal = computed(() => {
-  return useWindowSize().width.value < PC_DEVICE_WIDTH;
+  // return useWindowSize().width.value < PC_DEVICE_WIDTH;
+  return true;
 });
 
 export const useREM = () => {
--- grace_demo/src/views/main/components/navigation/mobile/index.vue
+++ grace_demo/src/views/main/components/navigation/mobile/index.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="bg-white sticky top-0 left-0 z-10">
+  <div class="bg-red dark:bg-zinc-900 duration-500 sticky top-0 left-0 z-10">
     <ul
-      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden bg-red"
+      ref="ulTarget"
     >
       <!-- 汉堡按钮 -->
       <li
-        class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
+        class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white dark:bg-zinc-900 shadow-l-white dark:shadow-l-zinc"
       >
         <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
       </li>
       <!-- 滑块 -->
       <li
         ref="sliderTarget"
-        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        class="absolute h-[22px] bg-zinc-900 dark:bg-zinc-800 rounded-lg duration-200"
         :style="sliderStyle"
       ></li>
       <!-- category item -->
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
-        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4 bg-red"
+        :class="{
+          'text-zinc-100 ': currentCategoryIndex === index,
+        }"
       >
         {{ item.name }}
       </li>
@@ -27,7 +33,9 @@
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, watch, onBeforeUpdate } from "vue";
+import { useScroll } from "@vueuse/core";
+
 defineProps({
   data: {
     type: Array,
@@ -38,5 +46,34 @@ defineProps({
 const sliderStyle = ref({
   transform: "translateX(0px)",
   width: "60px",
+});
+const currentCategoryIndex = ref(0);
+let itemRefs = [];
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el);
+  }
+};
+
+watch(currentCategoryIndex, (index) => {
+  const target = itemRefs[index];
+  if (target) {
+    const { left, width } = target.getBoundingClientRect();
+    sliderStyle.value = {
+      transform: `translateX(${ulScrollLeft.value + left - 10 + "px"})`,
+      width: `${width}px`,
+    };
+  }
+});
+
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index;
+};
+
+const ulTarget = ref(null);
+const { x: ulScrollLeft } = useScroll(ulTarget);
+
+onBeforeUpdate(() => {
+  itemRefs = [];
 });
 </script>
