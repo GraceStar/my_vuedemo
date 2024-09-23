fff648e14447a1fe613923546a94f157aba4d659
Grace <luoyawen99@gmail.com>
2024-09-23 15:12:53 +0800

    feat: vite通用组件自动化注册

--- grace_demo/src/libs/index.js
+++ grace_demo/src/libs/index.js
@@ -1,9 +1,11 @@
-import svgIcon from "./svg-icon/index.vue";
-import popup from './popup/index.vue';
+import { defineAsyncComponent } from "vue";
 
 export default {
   install: (app) => {
-    app.component("m-svg-icon", svgIcon);
-    app.component("m-popup", popup);
+      const components = import.meta.glob('./*/index.vue')
+      for(const[key, value] of Object.entries(components)){
+        const componentName = 'm-' + key.replace('./', '').split('/')[0];
+        app.component(componentName, defineAsyncComponent(value));
+      }
   },
 };
